049217c70149f9e5aa16660d722895adfffd01dc
Leidy Acuña <ing.leidyacunagarcia@gmail.com>
2020-12-27 10:35:42 -0500

    Schema extra examples

--- main.py
+++ main.py
@@ -1,62 +1,51 @@
-from typing import Optional, List, Set, Dict
+from typing import Optional
 
-from fastapi import FastAPI, Path, Query, Body
-from pydantic import BaseModel, Field, HttpUrl
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
 
 class Item(BaseModel):
-    name: str
-    description: Optional[str] = Field(None, title="description", max_length=300)
-    price: float = Field(..., gt=0, description="price")
-    tax: Optional[float] = None
-    tags: List[str] = []
-    tags_unique: Set[str] = set()
-    image: Optional[List[Image]] = None
-    
-class Offer(BaseModel):
+    name: str = Field(..., example="Foo")
+    description: Optional[str] = Field(None, example="A very nice Item")
+    price: float = Field(..., example=35.4)
+    tax: Optional[float] = Field(None, example=3.2)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.2
+            }
+        } 
+
+""" class Item(BaseModel):
     name: str
     description: Optional[str] = None
     price: float
-    items: List[Item]
-
-
-class User(BaseModel):
-    username: str
-    full_name: Optional[str] = None
-
-
-@app.post("/offers")
-async def create_offer(offer: Offer):
-    return offer
-
-@app.post("/images/multiple")
-async def create_multiple_images(images: List[Image]):
-    return images
-
-@app.post("/index-weights")
-async def create_index_weights(weights: Dict[int, float]):
-    return weights
-
-@app.post("/items/{item_id}")
-async def post_item(item_id: int, item: Item = Body(..., embed=True)):
+    tax: Optional[float] = None """
+    
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
     results = {"item_id": item_id, "item": item}
     return results
 
 
-@app.put("/items/{item_id}")
-async def update_item(*,
-                      item_id: int,
-                      item: Item,
-                      user: User,
-                      importance: int = Body(..., gt=0),
-                      q: Optional[str] = None
-                    ):
-    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
-    if q:
-        results.update({"q": q})
-    return results
+""" @app.put("/items/{item_id}")
+async def update_item(
+    item_id: int,
+    item: Item = Body(
+        ...,
+        example={
+            "name": "Foo",
+            "description": "A very nice Item",
+            "price": 35.4,
+            "tax": 3.2
+        }
+    )
+    ):
+    results = {"item_id": item_id, "item": item}
+    return results """
